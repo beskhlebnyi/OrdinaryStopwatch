a30d8af7248f239edfa415b2d2caded28909fc67
be5 <beskhlebnyi@gmail.com>
2018-10-27 11:34:13 +0300

    add app logic

--- app/src/main/java/com/example/beskhlebnyi/ordinarystopwatch/MainActivity.java
+++ app/src/main/java/com/example/beskhlebnyi/ordinarystopwatch/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.beskhlebnyi.ordinarystopwatch;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView stopwatch ;
+    Button button;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
+    Handler handler;
+    int Seconds, Minutes, MilliSeconds ;
+    boolean isPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stopwatch = findViewById(R.id.stopwatch);
+        button = findViewById(R.id.button);
+        handler = new Handler() ;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isPressed == false) {
+                    isPressed = true;
+                    StartTime = SystemClock.uptimeMillis();
+                    handler.postDelayed(runnable, 0);
+                    button.setText("stop");
+                }
+                else{
+                    isPressed = false;
+                    button.setText("start");
+                    handler.removeCallbacks(runnable);
+                    stopwatch.setText(stopwatch.getText());
+                }
+
+            }
+        });
     }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+            Seconds = (int) (MillisecondTime / 1000);
+            Minutes = Seconds / 60;
+            Seconds = Seconds % 60;
+            MilliSeconds = (int) (MillisecondTime % 1000);
+            stopwatch.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+            handler.postDelayed(this, 0);
+
+        }
+
+    };
 }
